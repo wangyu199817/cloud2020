98e55b2db767e6c17dfae87f37fe3a76c75eecc4
wangyu <wangyu@zhongguxin.com>
2020-10-09 15:29:58 +0800

    zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZkMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @descipe:
+ * @author: WY
+ * @create: 2020/10/9
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZkMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZkMain80.class,args);
+    }
+}
